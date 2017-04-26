d689867d8a94464a2f5a8ce8b696a203992ed5db
acsattila <acsattila904@gmail.com>
2017-04-26 23:58:31 +0200

    first commit

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/express-serve-static-core/index.d.ts" />
+/// <reference path="modules/mime/index.d.ts" />
+/// <reference path="modules/serve-static/index.d.ts" />
